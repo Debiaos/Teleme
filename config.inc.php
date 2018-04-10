94df102be5e4112793ad6f9a4fc84b2328b5306a
Debiaos <35834989+Debiaos@users.noreply.github.com>
2018-04-10 16:59:42 +0800

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -3,8 +3,8 @@
 READ ME
 This file is define the constants
 */
-define('BOT_API_KEY', '495250274:AAH_VVpreOHjOh8VGfP6ryzeXPuE96HYH3c');
-define('BOT_USERNAME', 'debiao_bot');
+define('BOT_API_KEY', 'id:');
+define('BOT_USERNAME', '_bot');
 define('HOOK_URL', 'https://www.teleme.me/hook.php');
 define('DB_HOST', '');
 define('DB_USER', '');
