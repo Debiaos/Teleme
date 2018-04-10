9773f0c356bb6037c9104b2ef8080179b16665a5
Debiaos <35834989+Debiaos@users.noreply.github.com>
2018-04-10 16:48:20 +0800

    Add files via upload

---
+++ config.inc.php
@@ -0,0 +1,13 @@
+<?php
+/*
+READ ME
+This file is define the constants
+*/
+define('BOT_API_KEY', '495250274:AAH_VVpreOHjOh8VGfP6ryzeXPuE96HYH3c');
+define('BOT_USERNAME', 'debiao_bot');
+define('HOOK_URL', 'https://www.teleme.me/hook.php');
+define('DB_HOST', '');
+define('DB_USER', '');
+define('DB_PASS', '');
+define('DB_NAME', '');
+?>
---
+++ set.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * README
+ * This file is intended to set the webhook.
+ * Uncommented parameters must be filled
+ */
+
+// Load composer
+require_once __DIR__ . '/vendor/autoload.php';
+
+require_once __DIR__ . '/config.inc.php';
+
+// Add you bot's API key and name
+$bot_api_key  = BOT_API_KEY;
+$bot_username = BOT_USERNAME;
+
+// Define the URL to your hook.php file
+$hook_url     = HOOK_URL;
+
+try {
+    // Create Telegram API object
+    $telegram = new Longman\TelegramBot\Telegram($bot_api_key, $bot_username);
+
+    // Set webhook
+    $result = $telegram->setWebhook($hook_url);
+
+    // To use a self-signed certificate, use this line instead
+    //$result = $telegram->setWebhook($hook_url, ['certificate' => $certificate_path]);
+
+    if ($result->isOk()) {
+        echo $result->getDescription();
+    }
+} catch (Longman\TelegramBot\Exception\TelegramException $e) {
+    echo $e->getMessage();
+}
